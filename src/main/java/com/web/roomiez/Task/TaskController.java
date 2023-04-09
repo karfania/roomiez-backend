8e0fd67f4e8e62fa548ffc2a16025f2bda0bfb64
ChelyFernandezUSC <cmfernan@usc.edu>
2023-04-08 22:31:09 -0700

    task class updates

--- src/main/java/com/web/roomiez/Task/TaskController.java
+++ src/main/java/com/web/roomiez/Task/TaskController.java
@@ -1,4 +1,28 @@
 package com.web.roomiez.Task;
+//THIS CLASS CALLS FUNCTIONS DEFINED IN TASK SERVICE
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/tasks")
 public class TaskController {
+
+    private TaskService taskService;
+    @Autowired
+    public TaskController(TaskService taskService){
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> addTask(@RequestBody Task task) {
+        taskService.addTask(task);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+    @GetMapping
+    public String hello(){
+        return "hello";
+    }
+
 }
--- src/main/java/com/web/roomiez/Task/TaskService.java
+++ src/main/java/com/web/roomiez/Task/TaskService.java
@@ -1,4 +1,20 @@
 package com.web.roomiez.Task;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+//THIS CLASS DEFINES FUNCTIONS
+@Service
 public class TaskService {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public void addTask(Task task) {
+        taskRepository.save(task);
+    }
+    public String hello(){
+        return "hello";
+    }
+
+    //Create a new task and add it to the database
 }
